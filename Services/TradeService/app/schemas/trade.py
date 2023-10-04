8a43136a3c5c23984fded252386bc63df9568605
Denis Kepich <kyo473@yandex.ru>
2023-10-04 21:01:27 +0300

    add Enum Currency

--- Services/TradeService/app/schemas/trade.py
+++ Services/TradeService/app/schemas/trade.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel, Field
-from typing import Any
 from datetime import datetime
+from enum import Enum
 
 class TagBase(BaseModel):
     Description: str = Field(title='Описание', default='')
@@ -11,16 +11,19 @@ class Tag(TagBase):
 
 class TagCreate(TagBase):
     pass
-
+class Currency(str,Enum):
+    Bitcoin = "BTC"
+    Matic = "MATIC"
+    USDT = "USDT"
 class TradeBase(BaseModel):
     #buyer_id: int = Field(title='Идентификатор покупателя')
     #seller_id: int = Field(title='Идентификатор продавца')
     #buyer_address: str = Field(title='Адрес покупателя',default='')
     #seller_address: str = Field(title='Адрес продавца')
     price: int = Field(title='Цена', nullable=False)
-    currency: str = Field(title='Валюта', nullable=False)
+    currency: Currency = Field(title='Валюта', nullable=False)
     description: str = Field(title='Описание', default='')
-    #created_at: Any = Field(title='Дата создания', default=datetime.utcnow)
+    created_at: str = Field(title='Дата создания', default=datetime.utcnow().isoformat())
     #geo_tag_id: int = Field(title='Идентификатор гео-тега')
     #hide: str = Field(title='Скрыто', nullable=False)
 
